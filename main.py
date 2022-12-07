caef1d1da0fc616f64e91dc4099c98ea0fc8bd31
Phúc Khang <84757707+PhKhang@users.noreply.github.com>
2022-12-07 22:04:21 +0700

    ReducingmoreuneccessarycodeprettySureISpeltWrong

--- main.py
+++ main.py
@@ -22,28 +22,33 @@ pygame.display.set_caption("The pirate of The Seven Seas")
 
 img1 = pygame.transform.scale(pygame.image.load(
     os.path.join("pikachuOnDeskSqr.png")), (80, 80))  # Hinh anh va thu nho thanh 80x80px
-img2 = pygame.image.load(os.path.join("Group 1.png"))  # Hinh anh
 
 
+# Tao mang that bu, moi phan tu la MOT VUNG HINH VUONG de tu do to mau, in hinh,... len
 rects = [[0]*50 for i in range(0, 50)]
 for y in range(5):
     for x in range(6):
         rects[y][x] = (pygame.Rect(10 + x * 90, y * 90, OB_WIDTH, OB_HEIGHT))
 
 
-def draw_win(ob1, ob2, a=(0, 0)):
+def draw_win():
     WIN.fill((255, 255, 255))  # Lam trang nguyen man hinh
 
     dem = 0
     for y in range(0, 5):
         for x in range(0, 6):
-            if (map[y][x] == 0):  # To o 0 Bien
+
+            # To o 0 Bien
+            if (map[y][x] == 0):
+                # To moi VUNG HINH VUONG mau xanh
                 pygame.draw.rect(WIN, BLUE, rects[y][x])
 
-            if (map[y][x] == 1):  # To o 1 Cuop bien
+            # To o 1 Cuop bien
+            if (map[y][x] == 1):
                 pygame.draw.rect(WIN, RED, rects[y][x])
 
-            if (map[y][x] == 3):  # To o 3 Dao
+            # To o 3 Dao
+            if (map[y][x] == 3):
                 pygame.draw.rect(WIN, GREEN, rects[y][x])
 
             # To DO roi them hinh o duoc chon
@@ -59,26 +64,6 @@ def draw_win(ob1, ob2, a=(0, 0)):
             dem += 1
 
     pygame.display.update()
-
-
-def FollowMe(pops, fpos):
-    LERP_FACTOR = 0.05
-    minimum_distance = 25
-    maximum_distance = 100
-
-    target_vector = pygame.math.Vector2(*pops)
-    follower_vector = pygame.math.Vector2(*fpos)
-    new_follower_vector = pygame.math.Vector2(*fpos)
-
-    distance = follower_vector.distance_to(target_vector)
-    if distance > minimum_distance:
-        direction_vector = (target_vector - follower_vector) / distance
-        min_step = max(0, distance - maximum_distance)
-        max_step = distance - minimum_distance
-        step_distance = min_step + (max_step - min_step) * LERP_FACTOR
-        new_follower_vector = follower_vector + direction_vector * step_distance
-
-    return (new_follower_vector)
 
 
 def main():
@@ -112,7 +97,7 @@ def main():
 
                             dem += 1
 
-        draw_win(object1, object2)
+        draw_win()
 
     pygame.quit()
 
